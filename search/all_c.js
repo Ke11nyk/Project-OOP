5be0b12687a0c222d7887ac25507fd3a8368af46
Артем Вербицький <verbytskiiartem@gmail.com>
2024-03-04 17:33:03 +0200

    nword2

--- search/all_c.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['player_0',['Player',['../class_player.html',1,'']]],
-  ['playeranim_1',['PlayerAnim',['../class_gameplay.html#a5aa0d0c12e21079ebc7399da3be097d6',1,'Gameplay']]],
-  ['preexit_2',['PreExit',['../class_gameplay.html#a82f5de7693f00455b4473d163db3f41d',1,'Gameplay']]]
-];
